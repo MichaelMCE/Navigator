fd8a5f8b813f88b711b8385f37f129185d47ba65
MichaelMCE <mapei_@hotmail.com>
2025-10-08 22:46:46 +0100

    Add files via upload

--- src/commonGlue.h
+++ src/commonGlue.h
@@ -1,22 +1,27 @@
 #pragma once
-#ifndef _TEENSY_H_
-#define _TEENSY_H_
+#ifndef _COMMONGLUE_H_
+#define _COMMONGLUE_H_
 
 #include <Arduino.h>
+#include <unistd.h>
 #include "InternalTemperature.h"
 #include "IntervalTimer.h"
 #include "config.h"
+#include "displays.h"
 #include "touch.h"
 #include "encoders.h"
 #include "vfont/vfont.h"
 #include "palette.h"
 #include "gps.h"
+#include "fileio.h"
+#include "polyfile.h"
 #include "record.h"
 #include "map.h"
 #include "scene.h"
 #include "timedate.h"
 #include "cmd.h"
-
+#include "tiles.h"
+#include "poi.h"
 
 
 
@@ -43,6 +48,13 @@ void log_setRecordState (const int state);
 void log_setAcquisitionState (const int state);
 void log_reset ();
 int log_load (const char *filename);
+
+void log_runStep (const uint8_t step);
+void log_runStop ();
+void log_runSet (const uint32_t position);
+void log_runStart ();
+void log_runReset ();
+void log_runPause ();
 
 #endif
 
