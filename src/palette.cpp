462a9e1d17737bdef95499e20103aaa98bf0ad6b
MichaelMCE <mapei_@hotmail.com>
2025-10-22 15:05:45 +0100

    Add files via upload

--- src/palette.cpp
+++ src/palette.cpp
@@ -2,7 +2,8 @@
 
 #include <Arduino.h>
 #include "config.h"
-#include "vfont/vfont.h"
+//#include "vfont/vfont.h"
+#include "palette.h"
 
 
 
@@ -43,7 +44,7 @@ FLASHMEM void palette_init ()
 	paletteSet(COLOUR_PAL_DARKBLUE,			COLOUR_24TO16(0x0000BB));
 	paletteSet(COLOUR_PAL_SOFTBLUE,			COLOUR_24TO16(0x7296D3));
 	paletteSet(COLOUR_PAL_BLUE_SEA,			COLOUR_24TO16(0x508DC5));		/* blue, but not too dark nor too bright. eg; Glass Lite:Volume */
-	
+		
 	paletteSet(COLOUR_PAL_CYAN,				COLOUR_CYAN);
 	paletteSet(COLOUR_PAL_AQUA,				COLOUR_24TO16(0x00B7EB));
 	
@@ -76,7 +77,11 @@ FLASHMEM void palette_init ()
 	paletteSet(COLOUR_PAL_BROWN,			COLOUR_24TO16(0x804000));
 
 	paletteSet(COLOUR_PAL_BACKGROUND,		COLOUR_24TO16(0xC6D0A6));
-	paletteSet(COLOUR_PAL_WATER,            COLOUR_24TO16(0x405DB5));
+	//paletteSet(COLOUR_PAL_WATER,            COLOUR_24TO16(0x405DB5));
+	
+	paletteSet(COLOUR_PAL_WATER,            COLOUR_24TO16(0x99CCCC));
+	paletteSet(COLOUR_PAL_HEATH,            COLOUR_24TO16(0xd6d99f));
+	
 
 	paletteSet(COLOUR_PAL_PL08,				COLOUR_24TO16(0xB09050));
 	paletteSet(COLOUR_PAL_PL09,				COLOUR_24TO16(0xB09050));
@@ -111,5 +116,71 @@ FLASHMEM void palette_init ()
 	
 	//paletteSet(COLOUR_PAL_PLYEDGE,			COLOUR_24TO16(0x555555));
 	paletteSet(COLOUR_PAL_PLYEDGE,			COLOUR_24TO16(0x777777));
+
+	paletteSet(COLOUR_PAL_LDS,				COLOUR_24TO16(0x007DA5));
+	paletteSet(COLOUR_PAL_BACKGROUND,		COLOUR_24TO16(0xF1EEE9));
+	paletteSet(COLOUR_PAL_HEATH,            COLOUR_24TO16(0xd6d99f));
+	
+	paletteSet(COLOUR_PAL_Pitch,            COLOUR_24TO16(0x89d2ae));
+	paletteSet(COLOUR_PAL_Meadow,           COLOUR_24TO16(0xcfeca8));
+	
+	paletteSet(COLOUR_PAL_Park ,            COLOUR_24TO16(0xcdf6ca));
+	paletteSet(COLOUR_PAL_Residential,      COLOUR_24TO16(0xdddddd));
+	paletteSet(COLOUR_PAL_School,           COLOUR_24TO16(0xf0f0d8));
+	paletteSet(COLOUR_PAL_Industrail,       COLOUR_24TO16(0xdfd1d6));
+	paletteSet(COLOUR_PAL_Building,         COLOUR_24TO16(0xc1b0af));
+	paletteSet(COLOUR_PAL_Parking,          COLOUR_24TO16(0xf6eeb6));
+	paletteSet(COLOUR_PAL_SportsCentre,     COLOUR_24TO16(0xf0f0d8));
+	paletteSet(COLOUR_PAL_Construction,     COLOUR_24TO16(0xdfd1d6));
+	paletteSet(COLOUR_PAL_Church,           COLOUR_24TO16(0x007DA5));
+	paletteSet(COLOUR_PAL_Administration,   COLOUR_24TO16(0xefc8c8));
+	paletteSet(COLOUR_PAL_Playground,       COLOUR_24TO16(0xccfff1));
+	paletteSet(COLOUR_PAL_MixedForest,      COLOUR_24TO16(0xb3e2a8));
+
+	paletteSet(COLOUR_PAL_Cemetery,         COLOUR_24TO16(0xaacbaf));
+	paletteSet(COLOUR_PAL_GolfCourse,       COLOUR_24TO16(0xb5e3b5));
+	paletteSet(COLOUR_PAL_MilArea,          COLOUR_24TO16(0xffa8a8));
+
+	paletteSet(COLOUR_PAL_BrushScrub,       COLOUR_24TO16(0xcdf6ca));
+	paletteSet(COLOUR_PAL_Sand,             COLOUR_24TO16(0xfedf88));
+	paletteSet(COLOUR_PAL_MarshWetland,     COLOUR_24TO16(0xf1eee9));
+	paletteSet(COLOUR_PAL_Dam,              COLOUR_24TO16(0xbababa));
+	paletteSet(COLOUR_PAL_RetailArea,       COLOUR_24TO16(0xf0d9d9));
+	paletteSet(COLOUR_PAL_Hospital,         COLOUR_24TO16(0xf0f0d8));
+	paletteSet(COLOUR_PAL_Water,            COLOUR_24TO16(0x99cccc));
+	paletteSet(COLOUR_PAL_DeciduousForest,  COLOUR_24TO16(0xb3e2a8));
+	paletteSet(COLOUR_PAL_Stadium,          COLOUR_24TO16(0x33cc99));
+	paletteSet(COLOUR_PAL_AirfieldApron,    COLOUR_24TO16(0xe9d1ff));
+	paletteSet(COLOUR_PAL_AerodromeAirfield,COLOUR_24TO16(0xeae8e2));
+	paletteSet(COLOUR_PAL_Supermarket,      COLOUR_24TO16(0xf6cdd2));
+	paletteSet(COLOUR_PAL_Quarry,           COLOUR_24TO16(0xc5c3c3));
+	paletteSet(COLOUR_PAL_Sea,              COLOUR_24TO16(0x99cccc));
+	paletteSet(COLOUR_PAL_PowerStation,     COLOUR_24TO16(0xEC3055));
+	paletteSet(COLOUR_PAL_PedestrianArea,   COLOUR_24TO16(0xededed));
+	paletteSet(COLOUR_PAL_PlayingField,     COLOUR_24TO16(0x74dcba));
+	paletteSet(COLOUR_PAL_RailwayPlatform,  COLOUR_24TO16(0xcc99ff));
+	paletteSet(COLOUR_PAL_Pier,             COLOUR_24TO16(0xf2efe9));
+	paletteSet(COLOUR_PAL_Barracks,         COLOUR_24TO16(0xff8f8f));
+	paletteSet(COLOUR_PAL_Zoo,              COLOUR_24TO16(0xa4f3a1));
+	
+	paletteSet(COLOUR_PAL_Highway,          COLOUR_24TO16(0xeb989a));
+	paletteSet(COLOUR_PAL_Freeway,          COLOUR_24TO16(0x809bc0));
+	paletteSet(COLOUR_PAL_Road,             COLOUR_24TO16(0xfefeb3));
+	paletteSet(COLOUR_PAL_ArterialRoad,     COLOUR_24TO16(0xfdd6a4));
+	paletteSet(COLOUR_PAL_PrincipledHighway,COLOUR_24TO16(0xeb989a));
+	paletteSet(COLOUR_PAL_ResidentialStreet,COLOUR_24TO16(0xfefefe));
+	paletteSet(COLOUR_PAL_CollectorRoad,    COLOUR_24TO16(0xfefeb3));
+	paletteSet(COLOUR_PAL_ServiceRoad,      COLOUR_24TO16(0xfefefe));
+	paletteSet(COLOUR_PAL_LivingStreet ,    COLOUR_24TO16(0xbababa));
+	paletteSet(COLOUR_PAL_Roundabout,       COLOUR_24TO16(0xfefefe));
+	paletteSet(COLOUR_PAL_Path,             COLOUR_24TO16(0x30b520));
+	paletteSet(COLOUR_PAL_CountryRoad,      COLOUR_24TO16(0xfefeb3));
+	paletteSet(COLOUR_PAL_FootPath,         COLOUR_24TO16(0xbababa));
+	paletteSet(COLOUR_PAL_ServiceRoadRestricted, COLOUR_24TO16(0xf6d4d4));
+	paletteSet(COLOUR_PAL_Steps,            COLOUR_24TO16(0xff656a));
+	paletteSet(COLOUR_PAL_Cycleway,         COLOUR_24TO16(0x0080ff));
+	paletteSet(COLOUR_PAL_AirportRunway,    COLOUR_24TO16(0xbbbbcc));
+	paletteSet(COLOUR_PAL_Mooring,          COLOUR_24TO16(0xf2efe9));
+	
 }
 
