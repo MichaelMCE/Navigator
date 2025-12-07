4baee1a6cfffd682eec012b8077a979e954c0a0a
MichaelMCE <mapei_@hotmail.com>
2025-12-07 23:54:06 +0000

    Add files via upload

--- src/timedate.h
+++ src/timedate.h
@@ -25,6 +25,7 @@ void date_adjustTime4BST (gpsdata_t *data);
 void date_adjustUTCToTimeZone (utcTime_t *gps, uint16_t tz, uint8_t tzdir);
 int date_nWeekdayOfMonth (int week, int wday, int month, int year, struct tm *result);
 void date_formatDateTime (gpsdata_t *gps, char *buffer, const uint32_t len);
+void date_getAdjustedTime (gpsdata_t *data, dategps_t *date, timegps_t *time);
 
 #ifdef __cplusplus
 }
