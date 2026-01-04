1e81991860d992c4037a106382250b1a13913778
MichaelMCE <mapei_@hotmail.com>
2026-01-04 20:02:19 +0000

    Add files via upload

--- src/map.cpp
+++ src/map.cpp
@@ -32,16 +32,18 @@ FLASHMEM void map_init (vfont_t *vfont)
 	sceneResetViewport(&inst);
 	
 	map_setDetail(MAP_RENDER_POI, 1);
-	map_setDetail(MAP_RENDER_COMPASS, 1);
+	map_setDetail(MAP_RENDER_COMPASS, 0);
 	map_setDetail(MAP_RENDER_VIEWPORT, 1);
 	map_setDetail(MAP_RENDER_TRACKPOINTS, 1);
 	map_setDetail(MAP_RENDER_LOCGRAPTHIC, 1);
-	map_setDetail(MAP_RENDER_OVERLAY, 1);
+	map_setDetail(MAP_RENDER_MEASURE, 1);		// ruler
 	
-	map_setDetail(MAP_RENDER_SLEVELS, 1);
+	map_setDetail(MAP_RENDER_SLEVELS, 0);
 	map_setDetail(MAP_RENDER_SAVAIL, 1);
 	map_setDetail(MAP_RENDER_SWORLD, 1);
 	map_setDetail(MAP_RENDER_CONSOLE, 0);
+	
+	map_setDetail(MAP_RENDER_STRINGS, 1);
 }
 
 void map_render (trackRecord_t *trackRecord, const pos_rec_t *location, const float heading, const uint32_t flags)
@@ -67,8 +69,8 @@ void map_render (trackRecord_t *trackRecord, const pos_rec_t *location, const fl
 		if (flags&MAP_RENDER_TRACKPOINTS) sceneRenderTrackPoints(&inst, trackRecord);
 	if (inst.rstats.rflags.locgraphic)
 		if (flags&MAP_RENDER_LOCGRAPTHIC) sceneRenderLocGraphic(&inst);
-	if (inst.rstats.rflags.overlay)
-		if (flags&MAP_RENDER_OVERLAY)	  sceneRenderOverlay(&inst);
+	if (inst.rstats.rflags.measure)
+		if (flags&MAP_RENDER_MEASURE)	  sceneRenderMeasure(&inst);
 	if (inst.rstats.rflags.compass)
 		if (flags&MAP_RENDER_COMPASS)	  sceneRenderCompass(&inst);
 	if (inst.rstats.rflags.poi)
@@ -81,23 +83,111 @@ void map_setDetail (const uint32_t detail, uint32_t state)
 	
 	if (detail == MAP_RENDER_POI)
 		inst.rstats.rflags.poi = state;
+		
 	else if (detail == MAP_RENDER_COMPASS)
 		inst.rstats.rflags.compass = state;
+		
 	else if (detail == MAP_RENDER_TRACKPOINTS)
 		inst.rstats.rflags.trkPts = state;
+		
 	else if (detail == MAP_RENDER_SLEVELS)
 		inst.rstats.rflags.satlevels = state;
+		
 	else if (detail == MAP_RENDER_SAVAIL)
 		inst.rstats.rflags.satAvailability = state;
+		
 	else if (detail == MAP_RENDER_SWORLD)
 		inst.rstats.rflags.satWorld = state;
+		
 	else if (detail == MAP_RENDER_CONSOLE)
 		inst.rstats.rflags.console = state;
+		
 	else if (detail == MAP_RENDER_VIEWPORT)
 		inst.rstats.rflags.map = state;
+		
 	else if (detail == MAP_RENDER_LOCGRAPTHIC)
 		inst.rstats.rflags.locgraphic = state;
-	else if (detail == MAP_RENDER_OVERLAY)
-		inst.rstats.rflags.overlay = state;
+		
+	else if (detail == MAP_RENDER_MEASURE)
+		inst.rstats.rflags.measure = state;
+		
+	else if (detail == MAP_RENDER_STRINGS)
+		inst.rstats.rflags.strings = state;
+
+	else if (detail == MAP_RENDER_AREAS)
+		inst.rstats.rflags.mapFilled = state;
+		
+	else if (detail == MAP_RENDER_AREAS_OUTLINE)
+		inst.rstats.rflags.mapOutline = state;
+		
+	else if (detail == MAP_RENDER_PATHS)
+		inst.rstats.rflags.pathFilled = state;
+		
+	else if (detail == MAP_RENDER_PATHS_LINE)
+		inst.rstats.rflags.pathLine = state;
+		
+	else if (detail == MAP_RENDER_TITLE_BOUNDARY)
+		inst.rstats.rflags.tileOutline = state;
+
 	
+	/*inst->rstats.rflags.trackSpot = 0;
+	inst->rstats.rflags.trackPath = 1;
+	inst->rstats.rflags.trackLine = 0;*/
+}
+
+uint32_t map_getDetail (const uint32_t detail)
+{
+	if (detail == MAP_RENDER_POI)
+		return inst.rstats.rflags.poi;
+		
+	else if (detail == MAP_RENDER_COMPASS)
+		return inst.rstats.rflags.compass;
+		
+	else if (detail == MAP_RENDER_TRACKPOINTS)
+		return inst.rstats.rflags.trkPts;
+		
+	else if (detail == MAP_RENDER_SLEVELS)
+		return inst.rstats.rflags.satlevels;
+		
+	else if (detail == MAP_RENDER_SAVAIL)
+		return inst.rstats.rflags.satAvailability;
+		
+	else if (detail == MAP_RENDER_SWORLD)
+		return inst.rstats.rflags.satWorld;
+		
+	else if (detail == MAP_RENDER_CONSOLE)
+		return inst.rstats.rflags.console;
+		
+	else if (detail == MAP_RENDER_VIEWPORT)
+		return inst.rstats.rflags.map;
+		
+	else if (detail == MAP_RENDER_LOCGRAPTHIC)
+		return inst.rstats.rflags.locgraphic;
+		
+	else if (detail == MAP_RENDER_MEASURE)
+		return inst.rstats.rflags.measure;
+		
+	else if (detail == MAP_RENDER_STRINGS)
+		return inst.rstats.rflags.strings;
+
+	else if (detail == MAP_RENDER_AREAS)
+		return inst.rstats.rflags.mapFilled;
+		
+	else if (detail == MAP_RENDER_AREAS_OUTLINE)
+		return inst.rstats.rflags.mapOutline;
+		
+	else if (detail == MAP_RENDER_PATHS)
+		return inst.rstats.rflags.pathFilled;
+		
+	else if (detail == MAP_RENDER_PATHS_LINE)
+		return inst.rstats.rflags.pathLine;
+		
+	else if (detail == MAP_RENDER_TITLE_BOUNDARY)
+		return inst.rstats.rflags.tileOutline;
+
+	return 0;
+	
+	/*inst->rstats.rflags.trackSpot = 0;
+	inst->rstats.rflags.trackPath = 1;
+	inst->rstats.rflags.trackLine = 0;*/
 }
--- src/map.h
+++ src/map.h
@@ -6,24 +6,29 @@
 
 
 
-#define MAP_RENDER_VIEWPORT		0x001
-#define MAP_RENDER_TRACKPOINTS	0x002
-#define MAP_RENDER_LOCGRAPTHIC	0x004
-#define MAP_RENDER_OVERLAY		0x008
-#define MAP_RENDER_COMPASS		0x010
-#define MAP_RENDER_POI			0x020
-#define MAP_RENDER_SLEVELS		0x040
-#define MAP_RENDER_SAVAIL		0x080
-#define MAP_RENDER_SWORLD		0x100
-#define MAP_RENDER_CONSOLE		0x200
-
+#define MAP_RENDER_VIEWPORT			0x0001
+#define MAP_RENDER_TRACKPOINTS		0x0002
+#define MAP_RENDER_LOCGRAPTHIC		0x0004
+#define MAP_RENDER_MEASURE			0x0008
+#define MAP_RENDER_COMPASS			0x0010
+#define MAP_RENDER_POI				0x0020
+#define MAP_RENDER_SLEVELS			0x0040
+#define MAP_RENDER_SAVAIL			0x0080
+#define MAP_RENDER_SWORLD			0x0100
+#define MAP_RENDER_CONSOLE			0x0200
+#define MAP_RENDER_STRINGS			0x0400
+#define MAP_RENDER_AREAS			0x0800
+#define MAP_RENDER_AREAS_OUTLINE	0x1000
+#define MAP_RENDER_PATHS			0x2000
+#define MAP_RENDER_PATHS_LINE		0x4000
+#define MAP_RENDER_TITLE_BOUNDARY	0x8000
 
 
 
 void map_init (vfont_t *vfont);
 void map_render (trackRecord_t *trackRecord, const pos_rec_t *location, const float heading, const uint32_t flags);
 void map_setDetail (const uint32_t detail, uint32_t state);
-
+uint32_t map_getDetail (const uint32_t detail);
 
 
 #endif
