4994474eebbe2c5cf4b1e757b5387edc73b2047f
MichaelMCE <mapei_@hotmail.com>
2026-02-01 20:10:54 +0000

    Add files via upload

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -211,6 +211,9 @@ FLASHMEM static void cmd_receiver (char *msg, const int cmdlen)
 			gps_pollInf(INF_PROTO_USER2);
 		else if (!strcmp(proto, "user3"))
 			gps_pollInf(INF_PROTO_USER3);
+	
+	}else if (!strncmp(msg, "savecfg", 7)){
+		gps_saveConfig();
 
 	}else if (!strncmp(msg, "rate:", 5)){
 		uint8_t rate = atoi(&msg[5]);
