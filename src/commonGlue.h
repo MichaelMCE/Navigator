e364496d69109c67b5bc08a195f2dea5bbfa450d
MichaelMCE <mapei_@hotmail.com>
2026-02-15 08:37:30 +0000

    Add files via upload

--- src/commonGlue.h
+++ src/commonGlue.h
@@ -24,11 +24,22 @@
 #include "poi.h"
 #include "ui/ui.h"
 #include "powersave.h"
+#include "clock.h"
 
 
 #if ENABLE_MTP
 #include "mtp.h"
 #endif
+
+
+
+#define RENDER_PAGE_MAP			1
+#define RENDER_PAGE_CLOCK		2
+#define RENDER_PAGE_SPECTRUM	3
+
+void page_set (const uint8_t page);
+uint8_t page_get ();
+
 
 
 #define LOG_WRITE_PERIOD		60	// write data to log once per n seconds
@@ -46,6 +57,8 @@ void log_pause ();
 void log_stop ();
 void log_start ();
 int log_load (const char *filename);
+int log_runStatus ();
+int log_stateIsPaused ();
 
 void log_runStep (const uint8_t step);
 void log_runStop ();
@@ -68,6 +81,11 @@ void render_zoomOut ();
 void render_zoomReset ();
 
 void drawPanel (const uint8_t which);
+uint16_t *drawPanel_getPixels (const uint8_t which);
+
+void render_screenBlank ();
+
+
 
 int uiInput (const int32_t x, const int32_t y, const uint32_t flags);
 
@@ -75,6 +93,8 @@ int uiInput (const int32_t x, const int32_t y, const uint32_t flags);
 #if defined(__IMXRT1062__)
 extern "C" uint32_t set_arm_clock (uint32_t frequency);
 #endif
+
+
 
 
 
